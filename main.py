ef475b741250bc15d5108c1e9e914aae5d5be5e4
Pc-3 <mrshevchenko33@gmail.com>
2024-02-29 16:58:13 +0200

    '_'

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ string_punctuation = string.punctuation + ' '
 my_value = input('Enter name of value: ')
 contains_letter = False
 
-
 if my_value in keyword.kwlist:
     contains_letter = False
 else:
     for i in my_value:
-        if i in string.ascii_lowercase:
+        if i in string.ascii_lowercase or i == '_':
             contains_letter = True
         if i in string.ascii_uppercase or my_value[0].isdigit() or i in string_punctuation and i != '_':
             contains_letter = False
-            break
+
 
 if contains_letter:
     print(True)
